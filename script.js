42c5bdef8ee2ed03c0595454c14f14857e236a95
shin <121587677+shinkoushi@users.noreply.github.com>
2024-05-11 18:48:14 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,127 @@
+const playBoard = document.querySelector(".play-board");
+const scoreElement = document.querySelector(".score");
+const highScoreElement = document.querySelector(".high-score");
+const controls = document.querySelectorAll(".controls i");
+
+let gameOver = false;
+let foodX, foodY;
+let snakeX = 5, snakeY = 5;
+let velocityX = 0, velocityY = 0;
+let snakeBody = [];
+let setIntervalId;
+let score = 0;
+
+
+
+// Getting high score from the local storage
+let highScore = localStorage.getItem("high-score") || 0;
+highScoreElement.innerText = `Score Tertinggi: ${highScore}`;
+
+const updateFoodPosition = () => {
+    // Passing a random 1 - 30 value as food position
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Math.floor(Math.random() * 30) + 1;
+}
+
+const audioElement = new Audio('audio/game-over.mp3'); // replace with the actual path to your audio file
+
+const handleGameOver = () => {
+  // Clearing the timer and reloading the page on game over
+  if (setIntervalId) { // check if setIntervalId is defined
+    clearInterval(setIntervalId);
+  }
+
+  Swal.fire({
+    imageUrl: "images/game-over.png",
+    imageWidth: 100,
+    imageHeight: 100,
+    title: "HAHA KALAH😆👎",
+    text: "coba lagi ya !",
+    didOpen: () => {
+      try {
+        audioElement.play(); // play the audio file
+      } catch (error) {
+        console.error("Error playing audio:", error);
+      }
+    },
+    didClose: () => {
+      audioElement.pause(); // pause the audio when the modal is closed
+      audioElement.currentTime = 0; // reset the audio to the beginning
+    }
+  }).then(() => {
+    location.reload();
+  });
+}
+const changeDirection = e => {
+    // Changing velocity value based on key press
+    if(e.key === "ArrowUp" && velocityY != 1) {
+        velocityX = 0;
+        velocityY = -1;
+    } else if(e.key === "ArrowDown" && velocityY != -1) {
+        velocityX = 0;
+        velocityY = 1;
+    } else if(e.key === "ArrowLeft" && velocityX != 1) {
+        velocityX = -1;
+        velocityY = 0;
+    } else if(e.key === "ArrowRight" && velocityX != -1) {
+        velocityX = 1;
+        velocityY = 0;
+    }
+}
+
+// Calling changeDirection on each key click and passing key dataset value as an object
+controls.forEach(button => button.addEventListener("click", () => changeDirection({ key: button.dataset.key })));
+
+const scoreAudioElement = new Audio('audio/hap.mp3'); 
+const initGame = () => {
+    if(gameOver) return handleGameOver();
+    let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+
+    // Checking if the snake hit the food
+    let previousScore = score;
+    if(snakeX === foodX && snakeY === foodY) {
+        updateFoodPosition();
+        snakeBody.push([foodY, foodX]); // Pushing food position to snake body array
+        score++; // increment score by 1
+        highScore = score >= highScore ? score : highScore;
+        localStorage.setItem("high-score", highScore);
+        scoreElement.innerText = `Score: ${score}`;
+        highScoreElement.innerText = `Score Tertinggi: ${highScore}`;
+
+        // Play audio if score has increased
+        if (previousScore < score) {
+            try {
+                scoreAudioElement.play(); // play the audio file
+            } catch (error) {
+                console.error("Error playing audio:", error);
+            }
+        }
+    }
+    // Updating the snake's head position based on the current velocity
+    snakeX += velocityX;
+    snakeY += velocityY;
+    
+    
+    for (let i = snakeBody.length - 1; i > 0; i--) {
+        snakeBody[i] = snakeBody[i - 1];
+    }
+    snakeBody[0] = [snakeX, snakeY]; 
+   
+    if(snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
+        return gameOver = true;
+    }
+
+    for (let i = 0; i < snakeBody.length; i++) {
+       
+        html += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+       
+        if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
+            gameOver = true;
+        }
+    }
+    playBoard.innerHTML = html;
+}
+
+updateFoodPosition();
+setIntervalId = setInterval(initGame, 100);
+document.addEventListener("keyup", changeDirection);
